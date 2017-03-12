a1a78d5b926b51065ec8cb8e7877e51f8da5bddc
Moritz Schmidt <fusselwurm@gmail.com>
2017-03-12 16:29:36 +0100

    hack Steam avatar location

--- src/user/data.js
+++ src/user/data.js
@@ -151,6 +151,14 @@ module.exports = function (User) {
 				user['icon:bgColor'] = '#aaa';
 			}
 
+			var picture_host_regex = /^http:\/\/(media\.steampowered\.com|cdn\.akamai\.steamstatic\.com)\//;
+			if (user.picture) {
+				user.picture = user.picture.replace(picture_host_regex, 'https://gruppe-adler.de/');
+			}
+			if (user.uploadedpicture) {
+				user.uploadedpicture = user.uploadedpicture.replace(picture_host_regex, 'https://gruppe-adler.de/');
+			}
+
 			if (user.picture && user.picture === user.uploadedpicture) {
 				user.uploadedpicture = user.picture.startsWith('http') ? user.picture : nconf.get('relative_path') + user.picture;
 				user.picture = user.uploadedpicture;
